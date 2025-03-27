232185e894ffccea848b27e5349c9797df47710a
IRFAN-KHAN-git <irfanprof9@gmail.com>
2025-03-28 03:25:03 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:2000/ai/get-review', { code })
+    const response = await axios.post('https://ai-code-review-system-backend.onrender.com/ai/get-review', { code })
     setReview(response.data)
   }
 
